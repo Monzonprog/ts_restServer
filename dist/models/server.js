dfbb842d797a99b33ca17bb09e6b89cd0ace7093
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2023-12-04 19:57:35 +0100

    Create middlewares

--- dist/models/server.js
+++ dist/models/server.js
@@ -5,14 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const usuario_1 = __importDefault(require("../routes/usuario"));
+const cors_1 = __importDefault(require("cors"));
 class Server {
     constructor() {
         this.apiPaths = {
-            usuarios: '/api/usuarios'
+            usuarios: '/api/usuarios',
         };
         this.app = (0, express_1.default)();
         this.port = process.env.PORT || '8081';
+        this.middlewares();
         this.routes();
+    }
+    middlewares() {
+        //CORS
+        this.app.use((0, cors_1.default)());
+        //LECTURA DE BODY
+        this.app.use(express_1.default.json());
+        //CARPETA PÚBLICA
+        this.app.use(express_1.default.static('public'));
     }
     routes() {
         this.app.use(this.apiPaths.usuarios, usuario_1.default);
--- models/server.ts
+++ models/server.ts
@@ -1,27 +1,40 @@
 import express, { Application } from 'express';
-import userRoutes from '../routes/usuario'
+import userRoutes from '../routes/usuario';
+import cors from 'cors';
 
 class Server {
     private app: Application;
     private port: string;
     private apiPaths = {
-        usuarios: '/api/usuarios'
-    }
+        usuarios: '/api/usuarios',
+    };
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8081';
+        this.middlewares();
         this.routes();
     }
 
-    routes(){
+    middlewares() {
+        //CORS
+        this.app.use(cors());
+
+        //LECTURA DE BODY
+        this.app.use(express.json());
+
+        //CARPETA PÚBLICA
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
         this.app.use(this.apiPaths.usuarios, userRoutes);
     }
 
-    listen(){
-        this.app.listen(this.port, ()=>{
+    listen() {
+        this.app.listen(this.port, () => {
             console.log('Servidor corriendo en el puerto ' + this.port);
-        })
+        });
     }
 }
 
